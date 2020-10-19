216c7e9f0387f44343713c39dd69800d20db6826
MindaugasKluonis <B00063711@student.itb.ie>
2020-10-19 21:16:15 +0100

    Add files via upload
    Added CameraController and MouseController classes

---
+++ C#/Controllers/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed = 5f;
+    public float minBorderX, maxBorderX, minBorderY, maxBorderY;
+
+    private Camera _currentCamera;
+    private Vector3 cameraVelocity = Vector3.zero;
+
+    private void Awake()
+    {
+        _currentCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void UpdateCameraPosition()
+    {
+        Vector3 pos = _currentCamera.ScreenToViewportPoint(Input.mousePosition);
+
+        UpdateVerticalCameraPosition(pos);
+        UpdateHorizontalCameraPosition(pos);
+
+        if (cameraVelocity != Vector3.zero)
+            _currentCamera.transform.Translate(cameraVelocity * Time.smoothDeltaTime);
+    }
+
+    private void UpdateHorizontalCameraPosition(Vector3 pos)
+    {
+        float x = pos.x;
+
+        if(x < minBorderX)
+        {
+            cameraVelocity.x = -cameraSpeed;
+            return;
+        }
+
+        else if(x > maxBorderX)
+        {
+            cameraVelocity.x = cameraSpeed;
+            return;
+        }
+
+        cameraVelocity.x = 0f;
+    }
+
+    private void UpdateVerticalCameraPosition(Vector3 pos)
+    {
+        float y = pos.y;
+
+        if (y < minBorderY)
+        {
+            cameraVelocity.y = -cameraSpeed;
+            return;
+        }
+
+        else if (y > maxBorderY)
+        {
+            cameraVelocity.y = cameraSpeed;
+            return;
+        }
+
+        cameraVelocity.y = 0f;
+    }
+}
---
+++ C#/Controllers/MouseController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public bool IsClickEnabled { set; get; }
+    public bool IsMoveEnabled { set; get; }
+
+    private Vector3 mousePosition;
+    private Vector3 previousPosition;
+
+    public delegate void OnMousePositionChanged(Vector3 position);
+    public event OnMousePositionChanged OnMousePositionChangedEvent = delegate { };
+
+    public delegate void OnMousePositionClicked(Vector3 position);
+    public event OnMousePositionClicked OnMousePositionClickedEvent = delegate { };
+
+    private void Awake()
+    {
+        //default isEnable values
+        IsClickEnabled = true;
+        IsMoveEnabled = true;
+    }
+
+    private void Update()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        UpdateMouseHandlers();
+    }
+
+    private void UpdateMouseHandlers()
+    {
+        if (IsClickEnabled)
+        {
+            if(mousePosition != previousPosition)
+            {
+                OnMousePositionChangedEvent.Invoke(mousePosition);
+                previousPosition = mousePosition;
+            }
+        }
+
+        if (IsMoveEnabled)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnMousePositionClickedEvent.Invoke(mousePosition);
+            }
+        }
+    }
+}
